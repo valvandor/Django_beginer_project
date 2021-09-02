03496546203b48d949e1f53f36878de9ec4b5373
valvandor <sincov.vitalii@gmail.com>
2021-09-02 14:54:33 +0300

    created media dir in the project and registered it in the settings

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -17,9 +17,17 @@ from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
 
+# for distributing media files during the dev-mode
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', mainapp.products, name='product'),
     path('contact/', mainapp.contact, name='contact'),
     path('', mainapp.main, name='main'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
