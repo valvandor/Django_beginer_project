31a0e0440e62a0fa1a68bb3b06c5a5bdc2391acc
valvandor <sincov.vitalii@gmail.com>
2021-09-10 15:39:24 +0300

    created command fill_db for filling database from JSON with creating superuser

---
+++ geekshop/mainapp/management/commands/fill_db.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+from authapp.models import ShopUser
+from django.core.management.base import BaseCommand
+
+from mainapp.models import ProductCategory, Product
+
+JSON_PATH = 'mainapp/json'
+
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as infile:
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            new_category = ProductCategory(**category)
+            new_category.save()
+
+        products = load_from_json('products')
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product["category"]
+            # Получаем категорию по имени
+            _category = ProductCategory.objects.get(name=category_name)
+            # Заменяем название категории объектом
+            product['category'] = _category
+            new_product = Product(**product)
+            new_product.save()
+
+        # Удаляем пользователя, если он существует
+        ShopUser.objects.filter(username="valvandor", is_superuser=True).delete()
+        # Создаем суперпользователя при помощи менеджера модели
+        ShopUser.objects.create_superuser(username='valvandor', password='geekbrains', age=27)
