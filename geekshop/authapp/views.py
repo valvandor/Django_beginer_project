d33c812074aadfb92295cd98613c5346605f0bc5
valvandor <sincov.vitalii@gmail.com>
2021-09-10 17:32:39 +0300

    Implemented an authentication mechanism in the project

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from authapp.forms import ShopUserLoginForm
+from django.urls import reverse
 
-# Create your views here.
+
+def login(request):
+    title = 'вход'
+
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+    content = {'title': title, 'login_form': login_form}
+    return render(request, 'authapp/login.html', content)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
-    path('auth', include('authapp.urls'))
+    path('auth/', include('authapp.urls', namespace='auth'))
 ]
 
 if settings.DEBUG:
