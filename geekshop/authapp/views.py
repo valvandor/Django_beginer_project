2c9e33f942fde28853422a808c112a92533ee76c
valvandor <sincov.vitalii@gmail.com>
2021-09-12 17:06:06 +0300

    made registration mechanism

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,11 +1,12 @@
 from django.contrib import auth
 from django.shortcuts import render, HttpResponseRedirect
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
 from django.urls import reverse
 
 
 def login(request):
     title = 'вход'
+    heading = 'вход'
 
     login_form = ShopUserLoginForm(data=request.POST)
     if request.method == 'POST' and login_form.is_valid():
@@ -17,11 +18,27 @@ def login(request):
             auth.login(request, user)
             return HttpResponseRedirect(reverse('index'))
 
-    content = {'title': title, 'login_form': login_form}
+    content = {'title': title, 'heading': heading, 'form': login_form}
     return render(request, 'authapp/login.html', content)
 
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    title = 'регистрация'
+    heading = 'регистрация'
+
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    content = {'title': title, 'heading': heading, 'form': register_form}
+    return render(request, 'authapp/register.html', content)
 
