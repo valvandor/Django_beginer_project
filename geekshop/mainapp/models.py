8488ccf1f1e87c0371404e4d40515a05d5f0f00d
valvandor <sincov.vitalii@gmail.com>
2021-09-14 21:31:10 +0300

    implemented a mechanism for auto-generating products in the database

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -22,3 +22,38 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.name} ({self.category.name})"
 
+    @staticmethod  # FIXME what are the possible good options to leave it inside the class?
+    def _get_image(img_url, file_name):
+        from django.conf import settings
+        import requests
+
+        path_for_save = f'{settings.MEDIA_ROOT}/products_images/'
+        f = open(f'{path_for_save}{file_name}.jpg', 'wb')
+        f.write(requests.get(img_url).content)
+        f.close()
+        path_for_get = f'products_images/{file_name}.jpg'
+        return path_for_get
+
+    @staticmethod
+    def _bootstrap(count=10, locale='ru'):
+        from mimesis import Internet
+        from random import randint
+        from mimesis import Text
+        from mimesis.random import get_random_item
+
+        categories = ProductCategory.objects.all()
+        img_url = Internet().stock_image(width=300, height=400)
+
+        for _ in range(count):
+            image_name = Text('en').word()
+
+            product = Product(
+                category=get_random_item(categories),
+                name=Text('ru').word(),
+                image=Product._get_image(img_url, image_name),  # FIXME this is not good
+                short_desc=' '.join(Text('ru').words(7)),
+                description=' '.join(Text('ru').words(17)),
+                price=randint(100, 10000),
+                quantity=randint(1, 100)
+            )
+            product.save()
