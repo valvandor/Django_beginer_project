1fbdbee78693bd209742877403b659056489ca9c
valvandor <sincov.vitalii@gmail.com>
2021-09-20 15:49:51 +0300

    implemented a mechanism for accessing the basket for authenticated users

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def basket(request):
     title = 'корзина'
     basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
@@ -14,7 +18,10 @@ def basket(request):
     return render(request, 'basketapp/basket.html', content)
 
 
+@login_required
 def basket_add(request, product_id):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product_detail', args=[product_id]))
     product = get_object_or_404(Product, pk=product_id)
     basket = Basket.objects.filter(user=request.user, product=product).first()
     if not basket:
@@ -24,6 +31,7 @@ def basket_add(request, product_id):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, product_id):
     # FIXME: add a confirmation mechanism with a form and sending data using the POST method
     basket_record = get_object_or_404(Basket, pk=product_id)
--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -135,3 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authapp.ShopUser'
+
+LOGIN_URL = '/auth/login/'
