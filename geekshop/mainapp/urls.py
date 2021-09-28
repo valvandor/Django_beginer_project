8217a7d2ff2a07091ab3c1e86abf17eae51a9ecb
valvandor <sincov.vitalii@gmail.com>
2021-09-28 14:05:05 +0300

    fixed a bug with viewing the product by changing the order of urls

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -6,6 +6,6 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products_main, name='products_main'),
+    path('<int:product_id>/', mainapp.product_detail, name='product_detail'),
     path('<slug:category_name>/', mainapp.products_category, name='products_category'),
-    path('<int:product_id>', mainapp.product_detail, name='product_detail'),
 ]
