f5e7ce759515f38b880f4d03c32174badc34a7ae
valvandor <sincov.vitalii@gmail.com>
2021-09-28 15:09:07 +0300

    made a mechanism for CRUD operations for working with user objects

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,5 +1,10 @@
-from authapp.models import ShopUser
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
+
+from adminapp.forms import ShopUserAdminEditForm
+from authapp.forms import ShopUserRegisterForm
+from authapp.models import ShopUser
 from mainapp.models import Product, ProductCategory
 
 
@@ -17,15 +22,52 @@ def users(request):
 
 
 def user_create(request):
-    pass
+    title = 'пользователи/создание'
+
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {'title': title, 'update_form': user_form}
+
+    return render(request, 'adminapp/user_update.html', content)
 
 
 def user_update(request, pk):
-    pass
+    title = 'пользователи/редактирование'
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {'title': title, 'update_form': edit_form}
+
+    return render(request, 'adminapp/user_update.html', content)
 
 
 def user_delete(request, pk):
-    pass
+    title = 'пользователи/удаление'
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        # вместо удаления — сделаем неактивным
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    content = {'title': title, 'user_to_delete': user}
+
+    return render(request, 'adminapp/user_delete.html', content)
 
 
 def categories(request):
